0af5dc7d3e0a0ebd6632145ac56853d795eea373
User Name <xiaojiecheng@Xiaojies-MacBook-Pro.local>
2024-11-06 11:45:46 -0500

    modified 3 files

--- js/app.js
+++ js/app.js
@@ -1,16 +1,16 @@
 const gameBoard = document.querySelector("#gameBoard");
 const ctx = gameBoard.getContext("2d");
 const scoreText = document.querySelector("#scoreText");
-const resetBtn = document.querySelector("#resetBtn");
+const startBtn = document.querySelector("#startBtn");
 const playerSelected = document.querySelector("#selected")
-const startBtn =document.querySelector('#startBtn')
+
 
 const gameWidth = gameBoard.width;
 const gameHeight = gameBoard.height;
-const boardBackground = "rgb(184, 243, 184)";
-const snakeColor = "red";
-const snakeBorder = "#6fc2f7";
-const foodColor = "blue";
+const boardBackground = "#b4fcde";
+const snakeColor = "#6a99ef";
+const snakeBorder = "yellow";
+const foodColor = "red";
 const unitSize = 25;
 
 
@@ -21,6 +21,7 @@ let ySpeed = 0;
 let foodX;
 let foodY;
 let score = 0;
+//set snake length
 let snake = [
     {x:unitSize * 4, y:0},
     {x:unitSize * 3, y:0},
@@ -29,55 +30,53 @@ let snake = [
     {x:0, y:0}
 ];
 
+
+// addEvenListener :direction,statBtn,playerSelcted
 window.addEventListener("keydown", changeDirection);
 startBtn.addEventListener('click',gameStart);
-resetBtn.addEventListener("click", resetGame);
 playerSelected.addEventListener('change',() => {
     setSpeed();
 });
 
-
-// set 4 kinds of speeds for player choosing 
-
-function setSpeed(){
-    switch(playerSelected.value)
-    {
-        case 'easy':
-            snakeSpeed = 300;
-            break;
-        case 'normal':
-            snakeSpeed = 200;
-            break;
-        case 'difficulty':
-            snakeSpeed = 150;
-            break;
-        case 'hell':
-            snakeSpeed = 70;
-            break;
-    }
+//if game start we need these functions work.
+function gameStart(){
+    if(running) return;
+      running =true;
+    
+    score = 0;
+    xSpeed = unitSize;
+    ySpeed = 0;
+    snake =[
+        {x:unitSize*4,y:0},   
+        {x:unitSize*3,y:0},   
+        {x:unitSize*2,y:0},   
+        {x:unitSize,y:0},   
+        {x:0,y:0},   
+    ]
+    scoreText.textContent = score;
+     createFood();
+     drawFood();
+     setSpeed();
+     nextTick();
+     
+};
+function resetGame(){
+    running = false; //stop the current game loop
+    clearBoard();    //
+    gameStart();     //restart the game with cleared board
 }
 
 
-
-function gameStart(){
-    running=true;
-    scoreText.textContent = score;
-    createFood();
-    drawFood();
-    nextTick();
-    setSpeed();
-};
-
+//nextTick make a loop.
 function nextTick(){
     if(running){
         setTimeout(()=>{
             clearBoard();
             drawFood();
-            moveSnake();
             drawSnake();
+            moveSnake();
             checkGameOver();
             nextTick();
-            setSpeed();
         }, snakeSpeed);
     }
     else{
@@ -85,72 +84,33 @@ function nextTick(){
     }
 };
     
+
+// clear gameboard draw a square gameboard with lightgreen color,same with the gameboard background color.
 function clearBoard(){
     ctx.fillStyle = boardBackground;
     ctx.fillRect(0, 0, gameWidth, gameHeight);
 };
 
-// function createFood(){
-//     let newFood;
-//     const occupiedPositions = new Set(snake.map(segment => `${segment.x},${segment.y}`));
-//     do{
-//         newFood ={
-//             x:Math.floor(Math.random()*20),
-//             y:Math.floor(Math.random()*20)
-//         };
-        
-//     } while (occupiedPositions.has(`${newFood.x},${newFood.y}`));
-
-//     foodX = newFood
-//     foodY = newFood
-
-// } ;
-
-
-
-function createFood(){
-    function randomFood(min, max){
-        const randNum = Math.round((Math.random() * (max - min) + min) / unitSize) * unitSize;
-        return randNum;
+// set 4 kinds of speeds for player choosing (260,180,120,70)
+function setSpeed(){
+    switch(playerSelected.value)
+    {
+        case 'easy':
+            snakeSpeed =260;
+            break;
+        case 'normal':
+            snakeSpeed = 180;
+            break;
+        case 'difficulty':
+            snakeSpeed = 120;
+            break;
+        case 'hell':
+            snakeSpeed = 70;
+            break;
     }
-
-     do{
-        foodX = randomFood(0,gameWidth - unitSize);
-        foodY = randomFood(0,gameWidth - unitSize);
-     } while(isFoodOnSnake(foodX,foodY));  
-};
-
-function isFoodOnSnake(x,y){
-    return snake.some(segment => segment.x ===x/unitSize && segment.y === y/unitSize);
 }
 
-function drawFood(){
-    ctx.fillStyle = foodColor;
-    ctx.fillRect(foodX, foodY, unitSize, unitSize);
-};
-function moveSnake(){
-    const head = {x: snake[0].x + xSpeed,
-                  y: snake[0].y + ySpeed};
-    
-    snake.unshift(head);
-    //if food is eaten
-    if(snake[0].x == foodX && snake[0].y == foodY){
-        score+=1;
-        scoreText.textContent = score;
-        createFood();
-    }
-    else{
-        snake.pop();
-    }     
-};
-function drawSnake(){
-    ctx.fillStyle = snakeColor;
-    ctx.strokeStyle = snakeBorder;
-    snake.forEach(snakePart => {
-        ctx.fillRect(snakePart.x, snakePart.y, unitSize, unitSize);
-        ctx.strokeRect(snakePart.x, snakePart.y, unitSize, unitSize);
-    })
-};
+// use arrow keys on the keyboard as buttons to change direction,x coordinate(left and right ),y coordinate(up and down).left and up was negative, right and down were positive.
 function changeDirection(event){
     const keyPressed = event.keyCode;
     const LEFT = 37;
@@ -182,6 +142,61 @@ function changeDirection(event){
             break;
     }
 };
+
+//draw and create random food and make sure that the food is not on the snake. 
+
+
+function createFood(){
+    function randomFood(min, max){
+        const randNum = Math.round((Math.random() * (max - min) + min) / unitSize) * unitSize;
+        return randNum;
+    }
+
+     do{
+        foodX = randomFood(0,gameWidth - unitSize);
+        foodY = randomFood(0,gameWidth - unitSize);
+     } while(isFoodOnSnake(foodX,foodY));  
+};
+function isFoodOnSnake(foodX,foodY){
+    return snake.some(segment => segment.x ===foodX && segment.y === foodY);
+};
+
+function drawFood(){
+    ctx.fillStyle = foodColor;
+    ctx.fillRect(foodX, foodY, unitSize, unitSize);
+};
+
+// draw snake and snakeborder with the color you like.
+
+function drawSnake(){
+    ctx.fillStyle = snakeColor;      //snakeColor you choose
+    ctx.strokeStyle = snakeBorder;  //snakeBorder color you  choose
+    snake.forEach(snakePart => {
+        ctx.fillRect(snakePart.x, snakePart.y, unitSize, unitSize);      //draw snake color 
+        ctx.strokeRect(snakePart.x, snakePart.y, unitSize, unitSize);    // draw snake board color
+    })
+};
+
+
+// when snake move ,add one head.
+function moveSnake(){
+    const head = {x: snake[0].x + xSpeed,
+                  y: snake[0].y + ySpeed};
+    
+    snake.unshift(head);
+    //if food is eaten
+    if(snake[0].x == foodX && snake[0].y == foodY){
+        score+=1;
+        scoreText.textContent = score;
+        createFood();
+    }
+    else{
+        snake.pop();
+    }     
+};
+
+
+
 function checkGameOver(){
     switch(true){
         case (snake[0].x < 0):
@@ -203,6 +218,8 @@ function checkGameOver(){
         }
     }
 };
+
+
 function displayGameOver(){
     ctx.font = "40px SimSuncss";
     ctx.fillStyle = "black";
@@ -210,19 +227,7 @@ function displayGameOver(){
     ctx.fillText("GAME OVER!", gameWidth / 2, gameHeight / 2);
     running = false;
 };
-function resetGame(){
-    score = 0;
-    xSpeed = unitSize;
-    ySpeed = 0;  
-    snake = [
-        {x:unitSize * 4, y:0},
-        {x:unitSize * 3, y:0},
-        {x:unitSize * 2, y:0},
-        {x:unitSize, y:0},
-        {x:0, y:0}
-    ];
-    gameStart();
-};
+
 
 
 
